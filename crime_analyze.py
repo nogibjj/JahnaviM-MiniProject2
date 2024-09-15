73e42e698a522ff4862eeb20a12c76b22ae36339
Jahnavi Maddhuri <jahnavi.maddhuri@gmail.com>
2024-09-15 12:18:25 -0400

    use pandas

--- crime_analyze.py
+++ crime_analyze.py
@@ -1,6 +1,11 @@
 '''This file is a sample python script to test the template and environement'''
 
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import plotly.express as px
+# import matplotlib.pyplot as plt
+# import plotly.express as px
+
+df = pd.read_csv('Crime_Data_from_2020_to_Present.csv')
+
+df['TimeOccHr'] = df['TIME OCC']//100 + df['TIME OCC']%100/60
+
+display(df.describe())
